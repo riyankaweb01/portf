c8b428645e6a7113d4e26726942e5c2c678b8ec0
riyankaweb01 <61414464+riyankaweb01@users.noreply.github.com>
2020-02-24 17:17:55 +0530

    Add files via upload

---
+++ js/include.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){ 
+	$(document).ready(function () {
+        $(".innimg").lightGallery({
+            share: false,
+            actualSize: false,
+			galleryId: 1
+        });
+    });
+});
+
